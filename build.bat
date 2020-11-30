4d7d9516b99d20f2e47ca1cbdc20e4d73efcbbf7
Mike Seese <seesemichaelj@gmail.com>
2020-11-29 22:25:47 -0800

    update build script

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ set LINUX_MULTIARCH_ROOT=C:\UnrealToolchains\v16_clang-9.0.1-centos7\
 
 "C:\Program Files\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" ^
   BuildPlugin ^
-  -Plugin=%CD%\Parser.uplugin ^
+  -Plugin=%CD%\ParseAndStringify.uplugin ^
   -Package=%CD%\Dist ^
-  -TargetPlatforms=Win64 ^
+  -TargetPlatforms=Win32+Win64+Mac+Linux ^
   -Rocket ^
   -VS2019
