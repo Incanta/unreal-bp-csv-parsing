6389b591dacdf3a2dbc41c4d88fb20c5427ca05a
Mike Seese <seesemichaelj@gmail.com>
2021-09-01 12:07:58 -0700

    use cross platform build script instead of batch file

---
+++ build.sh
@@ -0,0 +1,26 @@
+# only used in windows for cross compiling
+export LINUX_MULTIARCH_ROOT=/c/UnrealToolchains/v17_clang-10.0.1-centos7/
+if [ "$(uname)" != "Darwin" ]; then
+  ${LINUX_MULTIARCH_ROOT}x86_64-unknown-linux-gnu/bin/clang++ -v
+fi
+
+export ENGINE_VERSION=4.26
+export PLUGIN_NAME=$(basename $(pwd))
+
+if [ "$(uname)" == "Darwin" ]; then
+  UAT_PATH="/Users/Shared/Epic Games/UE_${ENGINE_VERSION}/Engine/Build/BatchFiles/RunUAT.sh"
+  TARGET_PLATFORMS=Mac
+else
+  # windows handles linux cross compile
+  UAT_PATH="/c/Program Files/Epic Games/UE_${ENGINE_VERSION}/Engine/Build/BatchFiles/RunUAT.bat"
+  TARGET_PLATFORMS=Win64+Linux+LinuxAArch64
+fi
+
+"${UAT_PATH}" \
+  BuildPlugin \
+  -Plugin=$(pwd)/${PLUGIN_NAME}.uplugin \
+  -Package=$(pwd)/Dist \
+  -TargetPlatforms=${TARGET_PLATFORMS} \
+  -Rocket \
+  -VS2017 \
+  | ../../compiler-beautify.pl --short
